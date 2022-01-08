f68d425126a857721c7bbb2c9810fae47f173ad4
kaku-you <74141219+kaku-you@users.noreply.github.com>
2022-01-08 14:27:12 +0900

    Add files via upload

---
+++ PerformanceManagement/OrderManagement/logic.py
@@ -0,0 +1,8 @@
+from .forms import EBIT_Calculator
+
+#class EBIT_Calculation(EBIT_Calculator):
+#    uriage = EBIT_Calculator.売上金額
+#    employeeFee = EBIT_Calculator.所員費用
+#    partnerFee = EBIT_Calculator.外注費用
+
+#    ararri = uriage - employeeFee - partnerFee*1.15
